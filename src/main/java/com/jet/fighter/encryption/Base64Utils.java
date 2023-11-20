387df31f479869750c98356d70e472dcc2421bc2
zhangdi <zhangdi@jtexpress.com.cn>
2023-11-20 17:23:13 +0800

    base64加盐加密

--- src/main/java/com/jet/fighter/encryption/Base64Utils.java
+++ src/main/java/com/jet/fighter/encryption/Base64Utils.java
@@ -18,6 +18,16 @@ import java.util.Base64;
  */
 public class Base64Utils {
 
+    /**
+     * 盐值
+     */
+    private static final String SALT = "华莱士";
+
+    /**
+     * 加密次数
+     */
+    private static final int REPEAT = 3;
+
     private Base64Utils() {
     }
 
@@ -65,11 +75,73 @@ public class Base64Utils {
         return decryptStr;
     }
 
+    /**
+     * Description: base64加盐加密
+     *
+     * @Author: di.zhang
+     * @Date: 2023/11/20 17:16
+     * @Param: java.lang.string
+     * @Return: java.lang.string
+     */
+    private static String saltEncrypt(String str) {
+        if (StringUtils.isEmpty(str)) {
+            throw new NullPointerException("加密字符不能为空");
+        }
+        byte[] bytes = new byte[0];
+        try {
+            //加盐
+            String tmpStr = str + SALT;
+            bytes = tmpStr.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        //多次加密
+        for (int i = 0; i < REPEAT; i++) {
+            bytes = Base64.getEncoder().encode(bytes);
+        }
+        return new String(bytes);
+    }
+
+    /**
+     * Description: base64加盐解密
+     *
+     * @Author: di.zhang
+     * @Date: 2023/11/13 15:59
+     * @Param: java.lang.string
+     * @Return: java.lang.string
+     */
+    public static String saltDecrypt(String str) {
+        if (StringUtils.isEmpty(str)) {
+            throw new NullPointerException("解密字符不能为空");
+        }
+        String decryptStr = null;
+        try {
+            byte[] bytes = str.getBytes();
+            //多次解密
+            for (int i = 0; i < 3; i++) {
+                bytes = Base64.getDecoder().decode(bytes);
+            }
+            decryptStr = new String(bytes, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        //删除盐值
+        return decryptStr.substring(0, decryptStr.length() - SALT.length());
+    }
+
     public static void main(String[] args) {
         String str = "测试base64加密";
         String encryptStr = encrypt(str);
         System.out.println("加密:" + encryptStr);
         String decryptStr = decrypt(encryptStr);
         System.out.println("解密:" + decryptStr);
+
+        String saltStr = "测试base64加密salt{}";
+        String saltEncryptStr = saltEncrypt(saltStr);
+        System.out.println("salt加密:" + saltEncryptStr);
+        String saltDecryptStr = saltDecrypt(saltEncryptStr);
+        System.out.println("salt解密:" + saltDecryptStr);
     }
 }
